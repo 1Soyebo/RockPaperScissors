ce113a2b8ece483fdea43cbbf20aa2058d8c79e9
1Soyebo <soyeboibukun@gmail.com>
2021-01-23 14:21:27 +0100

    feature:game's done

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -13,19 +13,27 @@ struct ContentView: View {
     @State private var computer_selection = Int.random(in: 0..<3)
     @State private var is_To_Win = Bool.random()
     @State private var isPresentingAlert = false
+    
+    @State private var alert_title = ""
+    @State private var alert_message = ""
+
+    
     @State private var isCorrect = false
     @State private var score = 0
     @State private var game_count = 0
     
     var hmmArray: [String]{
-        
         return array_options.filter({
             $0 != array_options[computer_selection]
         })
     }
     
     var body: some View {
+        
         VStack{
+            Text("Round \(game_count + 1)")
+                .padding()
+                .font(.title2)
             Text("Computer Chose \(array_options[computer_selection])")
                 .font(.title)
             Text("Guess \(is_To_Win ? "Right ✅":"Wrong ❌")")
@@ -36,24 +44,36 @@ struct ContentView: View {
             
             ForEach(0..<hmmArray.count){ number in
                 Button(action: {
+                    game_count += 1
                     isCorrect = determineRightAnswer() == hmmArray[number]
-                    isPresentingAlert = true
+                    presentAlertLogic()
                 }, label: {
                     Text(hmmArray[number]).padding()
                 
-                })
+                }).overlay(Capsule().stroke(Color.blue, lineWidth: 1))
             }
             
             Text("Score: \(score)/10")
                 .padding()
         }.alert(isPresented: $isPresentingAlert, content: {
-            Alert(title: Text(isCorrect ? "Correct":"Wrong"), message: Text("You \(isCorrect ? "Scored":"Lost") a point"), dismissButton: .default(Text("Ok")){
+            Alert(title: Text(alert_title), message: Text(alert_message), dismissButton: .default(Text("Ok")){
                 score = isCorrect ? score + 1 : score - 1
-                game_count += 1
                 array_options.shuffle()
                 is_To_Win = Bool.random()
             })
         })
+        
+    }
+    
+    func presentAlertLogic(){
+        if game_count == 10{
+            alert_title = "Game's Done"
+            alert_message = "Reset Game?"
+        }else{
+            alert_title = isCorrect ? "Correct":"Wrong"
+            alert_message = "You \(isCorrect ? "Scored":"Lost") a point"
+        }
+        isPresentingAlert = true
     }
     
     func determineRightAnswer() -> String{
